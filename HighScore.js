06d1f06ebb32eb463aca24384231ec8330d8a88a
Joshua Wilensky <joshwilensky@gmail.com>
2020-07-09 00:02:52 -0400

    git@github.com:joshwilensky/Homewor-4.git

---
+++ HighScore.js
@@ -0,0 +1,26 @@
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+var finalScore = document.querySelector("#finalScore")
+
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Retrieves local storage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(localStorage.getItem("allScores"));
+
+if (allScores !== null) {
+    console.log("I am Here")
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+// Event listener to move to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
